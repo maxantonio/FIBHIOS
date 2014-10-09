babe87d1c9f6c77a310199aff6c166c1dc7a6b99
Gran Dragon <dragon@3141681.com>
2014-10-08 23:21:54 -0400

    + directivos

--- app.rb
+++ app.rb
@@ -2,7 +2,21 @@
 get '/' do
   erb :index
 end
-
+# Nostros
 get '/perfil' do
     erb :perfil
+end
+get '/directivos' do
+    erb :directivos
+end
+
+
+
+
+
+
+
+
+get '/perfilb' do
+    erb :perfilb
 end
